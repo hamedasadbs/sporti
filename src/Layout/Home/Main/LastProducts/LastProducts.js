a0cccdf315a0bf5784cde1de5e9a8fc56157226c
hamedasad_bS <hamedasadollahi1998@gmail.com>
2021-10-21 23:18:49 +0330

    7/29

--- src/Layout/Home/Main/LastProducts/LastProducts.js
+++ src/Layout/Home/Main/LastProducts/LastProducts.js
@@ -2,20 +2,55 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 /*CSS*/
 import classes from "./LastProducts.module.scss";
 /*ASSETS*/
 import { Star, Favorite } from "@material-ui/icons";
 
-export const LastProducts = () => {
+export const LastProducts = (props) => {
   /*STATES*/
   const [productsData, setProductsData] = useState([]);
+  const [liked, setLiked] = useState([]);
   /*VARIABLES*/
   const url = "http://localhost/bsShop/lastProducts.php";
+  const likedURL = "http://localhost/bsShop/liked.php";
+  const isOnline = useSelector((state) => state.isOnline);
+  const accountName = useSelector((state) => state.accountName);
   /*FUNCTIONS*/
   useEffect(() => {
     axios.post(url).then((res) => setProductsData(res.data));
+    checkTheLiked();
   }, []);
+
+  const checkTheLiked = () => {
+    axios
+      .post(
+        likedURL,
+        JSON.stringify({
+          method: "checkTheLiked",
+          username: accountName,
+        })
+      )
+      .then((res) => setLiked(res.data));
+  };
+
+  const addToLiked = (id) => {
+    if (isOnline) {
+      axios
+        .post(
+          likedURL,
+          JSON.stringify({
+            method: "addToLiked",
+            username: accountName,
+            productId: id,
+          })
+        )
+        .then(() => {
+          checkTheLiked();
+        });
+    } else alert("ابتدا وارد حساب خود شوید");
+  };
 
   return (
     <>
@@ -25,7 +60,22 @@ export const LastProducts = () => {
           {productsData.map((res) => (
             <div className={classes.image} key={res.image}>
               <span className={classes.icons}>
-                <Favorite className={classes.liked} />
+                {liked.some((e) => e.product_id === res.id) ? (
+                  <Favorite
+                    onClick={() => {
+                      addToLiked(res.id);
+                    }}
+                    className={classes.liked}
+                  />
+                ) : (
+                  <Favorite
+                    onClick={() => {
+                      addToLiked(res.id);
+                    }}
+                    className={classes.notLiked}
+                  />
+                )}
+
                 <div className={classes.stars}>
                   {[...Array(5 - parseInt(res.population))].map((x) => (
                     <Star className={classes.star} />
