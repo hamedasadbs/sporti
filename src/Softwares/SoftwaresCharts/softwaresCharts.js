69af5799bad6dfe17c201cb69692b0795ae7b9a7
hamedasad_bS <hamedasadollahi1998@gmail.com>
2022-02-10 11:13:05 +0330

    11/21

--- src/Softwares/SoftwaresCharts/softwaresCharts.js
+++ src/Softwares/SoftwaresCharts/softwaresCharts.js
@@ -13,14 +13,19 @@ export const SoftwaresCharts = () => {
       <LineChart title="تعداد پاسخ ها" color="rgb(167, 108, 0)" id="2" />
       <LineChart title="نسبت موفقیت پاسخ ها" color="rgb(25, 100, 25)" id="3" />
       <BarChart
-        title="تعداد پاسخ های ارسال شده به"
-        color="rgb(167, 108, 0)"
+        title="تراکنش با سیستم های پر استفاده"
+        color="rgb(158, 38, 38)"
         id="1"
       />
       <BarChart
-        title="تعداد درخواست های دریافت شده از"
+        title="درخواست به سیستم های پر استفاده"
         color="rgb(90, 23, 90)"
         id="2"
+      />
+      <BarChart
+        title="دریافت رویداد و پاسخ از سیستم های پر تراکنش"
+        color="rgb(167, 108, 0)"
+        id="3"
       />
     </main>
   );
