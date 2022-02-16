05a7d384f9c7ca9cc1e4e30f654fa33630ef79da
hamedasad_bS <hamedasadollahi1998@gmail.com>
2022-02-16 23:35:17 +0330

    11/27

--- src/ToggleSwitch/toggleSwitch.js
+++ src/ToggleSwitch/toggleSwitch.js
@@ -1,16 +1,39 @@
+/*INNER-COMPONENTS*/
+import { useState } from "react";
 /*CSS*/
 import style from "./toggleSwitch.module.scss";
 
 export const ToggleSwitch = (props) => {
-  const modeHadler = (e) => {
-    if (e.target.checked) props.mode("dark");
-    else props.mode("light");
+  const [darkMode, setDarkMode] = useState(false);
+
+  const modeHadler = () => {
+    const switchVar = document.getElementsByClassName(style.switch)[0];
+    const brightIcon = document.getElementsByClassName(style.brightIcon)[0];
+    const darkIcon = document.getElementsByClassName(style.darkIcon)[0];
+
+    if (darkMode) {
+      props.mode("bright");
+      setDarkMode(false);
+      switchVar.style.marginLeft = "5px";
+      switchVar.style.backgroundColor = "rgb(0, 69, 136)";
+      brightIcon.style.display = "initial";
+      darkIcon.style.display = "none";
+    } else {
+      props.mode("dark");
+      setDarkMode(true);
+      switchVar.style.marginLeft = "30px";
+      switchVar.style.backgroundColor = "rgb(0, 14, 28)";
+      brightIcon.style.display = "none";
+      darkIcon.style.display = "initial";
+    }
   };
 
   return (
-    <label className={style.switch}>
-      <input onChange={modeHadler} type="checkbox" />
-      <span className={`${style.slider} ${style.round}`}></span>
-    </label>
+    <div onClick={modeHadler} className={style.toggle}>
+      <span className={style.switch}>
+        <i className={`fa fa-sun-o ${style.brightIcon}`}></i>
+        <i className={`fa fa-moon-o ${style.darkIcon}`}></i>
+      </span>
+    </div>
   );
 };
